57d0aadfcf52eaf26cdf7eba82e4f533c6df5ae3
James Rushford <james.rushford@simplesurance.de>
2020-09-22 10:08:16 +0200

    set city colors for armies

--- src/constants.js
+++ src/constants.js
@@ -7,15 +7,61 @@ export const deck = {
       attack: 9,
       defence: 1,
       imageUrl:
-        'https://cdn.britannica.com/34/83934-050-4787BA22/Roman-torsion-arm-stone-bundle-cords-force.jpg'
+        'https://cdn.britannica.com/34/83934-050-4787BA22/Roman-torsion-arm-stone-bundle-cords-force.jpg',
+      color: CITY_COLORS[0]
     },
-    { title: 'Armored Knight', attack: 5, defence: 9 },
-    { title: 'Archer', attack: 8, defence: 4 },
-    { title: 'Warrior', attack: 4, defence: 4 },
-    { title: 'Horseman', attack: 7, defence: 4 },
-    { title: 'Axeman', attack: 4, defence: 5 },
-    { title: 'Pikeman', attack: 5, defence: 8 },
-    { title: 'Mounted knight', attack: 9, defence: 6 }
+    { title: 'Armored Knight', attack: 5, defence: 9, color: CITY_COLORS[0] },
+    { title: 'Archer', attack: 8, defence: 4, color: CITY_COLORS[0] },
+    { title: 'Warrior', attack: 4, defence: 4, color: CITY_COLORS[0] },
+    { title: 'Horseman', attack: 7, defence: 4, color: CITY_COLORS[0] },
+    { title: 'Axeman', attack: 4, defence: 5, color: CITY_COLORS[0] },
+    { title: 'Pikeman', attack: 5, defence: 8, color: CITY_COLORS[0] },
+    { title: 'Mounted knight', attack: 9, defence: 6, color: CITY_COLORS[0] },
+    {
+      title: 'Catapult',
+      attack: 9,
+      defence: 1,
+      imageUrl:
+        'https://cdn.britannica.com/34/83934-050-4787BA22/Roman-torsion-arm-stone-bundle-cords-force.jpg',
+      color: CITY_COLORS[1]
+    },
+    { title: 'Armored Knight', attack: 5, defence: 9, color: CITY_COLORS[1] },
+    { title: 'Archer', attack: 8, defence: 4, color: CITY_COLORS[1] },
+    { title: 'Warrior', attack: 4, defence: 4, color: CITY_COLORS[1] },
+    { title: 'Horseman', attack: 7, defence: 4, color: CITY_COLORS[1] },
+    { title: 'Axeman', attack: 4, defence: 5, color: CITY_COLORS[1] },
+    { title: 'Pikeman', attack: 5, defence: 8, color: CITY_COLORS[1] },
+    { title: 'Mounted knight', attack: 9, defence: 6, color: CITY_COLORS[1] },
+    {
+      title: 'Catapult',
+      attack: 9,
+      defence: 1,
+      imageUrl:
+        'https://cdn.britannica.com/34/83934-050-4787BA22/Roman-torsion-arm-stone-bundle-cords-force.jpg',
+      color: CITY_COLORS[2]
+    },
+    { title: 'Armored Knight', attack: 5, defence: 9, color: CITY_COLORS[2] },
+    { title: 'Archer', attack: 8, defence: 4, color: CITY_COLORS[2] },
+    { title: 'Warrior', attack: 4, defence: 4, color: CITY_COLORS[2] },
+    { title: 'Horseman', attack: 7, defence: 4, color: CITY_COLORS[2] },
+    { title: 'Axeman', attack: 4, defence: 5, color: CITY_COLORS[2] },
+    { title: 'Pikeman', attack: 5, defence: 8, color: CITY_COLORS[2] },
+    { title: 'Mounted knight', attack: 9, defence: 6, color: CITY_COLORS[2] },
+    {
+      title: 'Catapult',
+      attack: 9,
+      defence: 1,
+      imageUrl:
+        'https://cdn.britannica.com/34/83934-050-4787BA22/Roman-torsion-arm-stone-bundle-cords-force.jpg',
+      color: CITY_COLORS[3]
+    },
+    { title: 'Armored Knight', attack: 5, defence: 9, color: CITY_COLORS[3] },
+    { title: 'Archer', attack: 8, defence: 4, color: CITY_COLORS[3] },
+    { title: 'Warrior', attack: 4, defence: 4, color: CITY_COLORS[3] },
+    { title: 'Horseman', attack: 7, defence: 4, color: CITY_COLORS[3] },
+    { title: 'Axeman', attack: 4, defence: 5, color: CITY_COLORS[3] },
+    { title: 'Pikeman', attack: 5, defence: 8, color: CITY_COLORS[3] },
+    { title: 'Mounted knight', attack: 9, defence: 6, color: CITY_COLORS[3] }
   ],
   cities: [
     { title: 'City Wall', benefit: 'defence', bonus: 1, color: CITY_COLORS[0] },
