ac3544c66072579aa155931de22b69e94a7b8268
James Rushford <james.rushford@simplesurance.de>
2020-10-01 21:30:45 +0200

    rewrite the deck fucntions

--- src/constants.js
+++ src/constants.js
@@ -1,159 +1,51 @@
 export const CITY_COLORS = ['red', 'lightblue', 'orange', 'pink']
 
-const cards = {
-  armies: [
-    {
-      title: 'Catapult',
-      attack: 9,
-      defence: 1,
-      imageUrl:
-        'https://cdn.britannica.com/34/83934-050-4787BA22/Roman-torsion-arm-stone-bundle-cords-force.jpg',
-      color: CITY_COLORS[0]
-    },
-    { title: 'Armored Knight', attack: 5, defence: 9, color: CITY_COLORS[0] },
-    { title: 'Archer', attack: 8, defence: 4, color: CITY_COLORS[0] },
-    { title: 'Warrior', attack: 4, defence: 4, color: CITY_COLORS[0] },
-    { title: 'Horseman', attack: 7, defence: 4, color: CITY_COLORS[0] },
-    { title: 'Axeman', attack: 4, defence: 5, color: CITY_COLORS[0] },
-    { title: 'Pikeman', attack: 5, defence: 8, color: CITY_COLORS[0] },
-    { title: 'Mounted knight', attack: 9, defence: 6, color: CITY_COLORS[0] },
-    {
-      title: 'Catapult',
-      attack: 9,
-      defence: 1,
-      imageUrl:
-        'https://cdn.britannica.com/34/83934-050-4787BA22/Roman-torsion-arm-stone-bundle-cords-force.jpg',
-      color: CITY_COLORS[1]
-    },
-    { title: 'Armored Knight', attack: 5, defence: 9, color: CITY_COLORS[1] },
-    { title: 'Archer', attack: 8, defence: 4, color: CITY_COLORS[1] },
-    { title: 'Warrior', attack: 4, defence: 4, color: CITY_COLORS[1] },
-    { title: 'Horseman', attack: 7, defence: 4, color: CITY_COLORS[1] },
-    { title: 'Axeman', attack: 4, defence: 5, color: CITY_COLORS[1] },
-    { title: 'Pikeman', attack: 5, defence: 8, color: CITY_COLORS[1] },
-    { title: 'Mounted knight', attack: 9, defence: 6, color: CITY_COLORS[1] },
-    {
-      title: 'Catapult',
-      attack: 9,
-      defence: 1,
-      imageUrl:
-        'https://cdn.britannica.com/34/83934-050-4787BA22/Roman-torsion-arm-stone-bundle-cords-force.jpg',
-      color: CITY_COLORS[2]
-    },
-    { title: 'Armored Knight', attack: 5, defence: 9, color: CITY_COLORS[2] },
-    { title: 'Archer', attack: 8, defence: 4, color: CITY_COLORS[2] },
-    { title: 'Warrior', attack: 4, defence: 4, color: CITY_COLORS[2] },
-    { title: 'Horseman', attack: 7, defence: 4, color: CITY_COLORS[2] },
-    { title: 'Axeman', attack: 4, defence: 5, color: CITY_COLORS[2] },
-    { title: 'Pikeman', attack: 5, defence: 8, color: CITY_COLORS[2] },
-    { title: 'Mounted knight', attack: 9, defence: 6, color: CITY_COLORS[2] },
-    {
-      title: 'Catapult',
-      attack: 9,
-      defence: 1,
-      imageUrl:
-        'https://cdn.britannica.com/34/83934-050-4787BA22/Roman-torsion-arm-stone-bundle-cords-force.jpg',
-      color: CITY_COLORS[3]
-    },
-    { title: 'Armored Knight', attack: 5, defence: 9, color: CITY_COLORS[3] },
-    { title: 'Archer', attack: 8, defence: 4, color: CITY_COLORS[3] },
-    { title: 'Warrior', attack: 4, defence: 4, color: CITY_COLORS[3] },
-    { title: 'Horseman', attack: 7, defence: 4, color: CITY_COLORS[3] },
-    { title: 'Axeman', attack: 4, defence: 5, color: CITY_COLORS[3] },
-    { title: 'Pikeman', attack: 5, defence: 8, color: CITY_COLORS[3] },
-    { title: 'Mounted knight', attack: 9, defence: 6, color: CITY_COLORS[3] }
-  ],
-  cities: [
-    { title: 'City Wall', benefit: 'defence', bonus: 1, color: CITY_COLORS[0] },
-    {
-      title: 'Town Hall',
-      benefit: 'handCapacity',
-      bonus: 1,
-      color: CITY_COLORS[0],
-      text: 'Benefits: plus 1 to hand capacity'
-    },
-    {
-      title: 'Armory',
-      benefit: 'colorAttack',
-      bonus: 1,
-      color: CITY_COLORS[0]
-    },
-    {
-      title: 'Forge',
-      benefit: 'colorDefence',
-      bonus: 1,
-      color: CITY_COLORS[0]
-    },
-    { title: 'City Wall', benefit: 'defence', bonus: 1, color: CITY_COLORS[1] },
-    {
-      title: 'Town Hall',
-      benefit: 'handCapacity',
-      bonus: 1,
-      color: CITY_COLORS[1],
-      text: 'Benefits: plus 1 to hand capacity'
-    },
-    {
-      title: 'Armory',
-      benefit: 'colorAttack',
-      bonus: 1,
-      color: CITY_COLORS[1]
-    },
-    {
-      title: 'Forge',
-      benefit: 'colorDefence',
-      bonus: 1,
-      color: CITY_COLORS[1]
-    },
-    { title: 'City Wall', benefit: 'defence', bonus: 1, color: CITY_COLORS[2] },
-    {
-      title: 'Town Hall',
-      benefit: 'handCapacity',
-      bonus: 1,
-      color: CITY_COLORS[2],
-      text: 'Benefits: plus 1 to hand capacity'
-    },
-    {
-      title: 'Armory',
-      benefit: 'colorAttack',
-      bonus: 1,
-      color: CITY_COLORS[2]
-    },
-    {
-      title: 'Forge',
-      benefit: 'colorDefence',
-      bonus: 1,
-      color: CITY_COLORS[2]
-    },
-    { title: 'City Wall', benefit: 'defence', bonus: 1, color: CITY_COLORS[3] },
-    {
-      title: 'Town Hall',
-      benefit: 'handCapacity',
-      bonus: 1,
-      color: CITY_COLORS[3],
-      text: 'Benefits: plus 1 to hand capacity'
-    },
-    {
-      title: 'Armory',
-      benefit: 'colorAttack',
-      bonus: 1,
-      color: CITY_COLORS[3]
-    },
-    {
-      title: 'Forge',
-      benefit: 'colorDefence',
-      bonus: 1,
-      color: CITY_COLORS[3]
-    }
-  ]
-}
-
-export const deck = [
-  ...cards.armies.map((card, index) => {
-    card.id = 'a' + index
-    return card
-  }),
-  ...cards.cities.map((card, index) => {
-    card.id = 'c' + index
-    return card
-  })
+const armies = [
+  {
+    title: 'Catapult',
+    attack: 9,
+    defence: 1,
+    imageUrl:
+      'https://cdn.britannica.com/34/83934-050-4787BA22/Roman-torsion-arm-stone-bundle-cords-force.jpg'
+  },
+  { title: 'Armored Knight', attack: 5, defence: 9 },
+  { title: 'Archer', attack: 8, defence: 4 },
+  { title: 'Warrior', attack: 4, defence: 4 },
+  { title: 'Horseman', attack: 7, defence: 4 },
+  { title: 'Axeman', attack: 4, defence: 5 },
+  { title: 'Pikeman', attack: 5, defence: 8 },
+  { title: 'Mounted knight', attack: 9, defence: 6 }
 ]
+const cities = [
+  { title: 'City Wall', benefit: 'defence', bonus: 1 },
+  {
+    title: 'Town Hall',
+    benefit: 'handCapacity',
+    bonus: 1,
+
+    text: 'Benefits: plus 1 to hand capacity'
+  },
+  {
+    title: 'Armory',
+    benefit: 'colorAttack',
+    bonus: 1
+  },
+  {
+    title: 'Forge',
+    benefit: 'colorDefence',
+    bonus: 1
+  }
+]
+
+const makeTeams = category =>
+  CITY_COLORS.map(color =>
+    category.map((item, index) => ({
+      ...item,
+      color,
+      key: `${item.attack ? 'a' : 'c'}-${color}-${index}`
+    }))
+  )
+
+const deckWithTeams = [...makeTeams(armies).flat(), ...makeTeams(cities).flat()]
+
+export const deck = deckWithTeams
