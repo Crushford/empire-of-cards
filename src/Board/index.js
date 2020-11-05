1eea44ca1be1ac1c68a640777eaa2ea846df30fa
James Rushford <james.rushford@simplesurance.de>
2020-11-05 21:39:57 +0100

    add feeback from at end of game

--- src/Board/index.js
+++ src/Board/index.js
@@ -101,6 +101,18 @@ export const Board = ({ G, ctx, moves, isMultiplayer, isActive, playerID }) => {
                 {parseInt(ctx.currentPlayer) === parseInt(ctx.gameover.winner)
                   ? 'Congratulations!'
                   : 'Better Luck Next Time'}
+                <br />
+                Thanks for playing, Empire of Cards is still a work in progress,
+                would you mind sending some feedback on the game?
+                <href
+                  src={`https://docs.google.com/forms/d/e/1FAIpQLSfK6zgugtFf0Lw3YVHjIannTkrY3LOWi2A9TIBeSoiHnH4-Tw/viewform?usp=pp_url&entry.661267773=${
+                    ctx.numPlayers
+                  }&entry.885079316=${
+                    G.isMultiplayer ? 'Multiplayer' : 'Single+Player'
+                  }&entry.993122104=${ctx.turn}`}
+                >
+                  Here is the link to the form
+                </href>
               </NextTurn>
               <AcceptTurn onClick={newGame}>New Game</AcceptTurn>
             </>
