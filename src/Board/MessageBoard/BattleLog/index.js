b52c406da1e31e4e684a8f7b8cac30e45b313514
James Rushford <james.rushford@simplesurance.de>
2020-11-11 10:19:49 +0100

    remove material ui for log

--- src/Board/MessageBoard/BattleLog/index.js
+++ src/Board/MessageBoard/BattleLog/index.js
@@ -1,22 +1,23 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import { Dialog, Typography, withStyles } from '@material-ui/core'
-import IconButton from '@material-ui/core/IconButton'
-import CloseIcon from '@material-ui/icons/Close'
-import MuiDialogTitle from '@material-ui/core/DialogTitle'
 
-const styles = theme => ({
-  root: {
-    margin: 0,
-    padding: theme.spacing(2)
-  },
-  closeButton: {
-    position: 'absolute',
-    right: theme.spacing(1),
-    top: theme.spacing(1),
-    color: theme.palette.grey[500]
-  }
-})
+const FullLog = styled.div`
+  height: 30%;
+  width: 30%;
+  background-color: 'black';
+  position: fixed;
+  top: calc(100% - (30% / 2));
+  left: calc(100% - (30% / 2));
+  z-index: 2;
+`
+
+const Shade = styled.div`
+  height: 100vh;
+  width: 100vw;
+  filter: blur(4px);
+  position: fixed;
+  z-index: 1;
+`
 
 const LatestMessages = styled.h5`
   color: purple;
@@ -29,23 +30,6 @@ const LogList = styled.ol`
   max-height: 300px;
   overflow-y: scroll;
 `
-const DialogTitle = withStyles(styles)(props => {
-  const { children, classes, onClose, ...other } = props
-  return (
-    <MuiDialogTitle disableTypography className={classes.root} {...other}>
-      <Typography variant="h6">{children}</Typography>
-      {onClose ? (
-        <IconButton
-          aria-label="close"
-          onClick={onClose}
-          className={classes.closeButton}
-        >
-          <CloseIcon />
-        </IconButton>
-      ) : null}
-    </MuiDialogTitle>
-  )
-})
 
 export const BattleLog = ({ log }) => {
   const [logOpen, setLogOpen] = useState(false)
@@ -71,16 +55,18 @@ export const BattleLog = ({ log }) => {
         Latest Battle Update: {log[log.length - 1]}
       </LatestMessages>
       <ShowLog onClick={openLog}>Show Entire Log</ShowLog>
-      <Dialog open={logOpen} onClose={closeLog}>
-        <DialogTitle id="customized-dialog-title" onClose={closeLog}>
-          Entire Log
-        </DialogTitle>
-        <LogList>
-          {log.map(message => (
-            <li>{message}</li>
-          ))}
-        </LogList>
-      </Dialog>
+      {logOpen && (
+        <Shade onclick={closeLog}>
+          <FullLog open={logOpen}>
+            <h3 id="customized-dialog-title">Entire Log</h3>
+            <LogList>
+              {log.map(message => (
+                <li>{message}</li>
+              ))}
+            </LogList>
+          </FullLog>
+        </Shade>
+      )}
     </>
   )
 }
